8701e3dad43467e48b4bb923c96ae0ff283f7d45
adthai <adthai@uw.edu>
2024-03-16 17:23:10 -0700

    adding work experience

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
-import Header from './components/Header/Header'
-import Hero from './components/hero/Hero'
-import css from './styles/App.module.scss'
+import Expertise from './components/expertise/expertise';
+import Header from './components/Header/Header';
+import Hero from './components/hero/Hero';
+import Work from './components/Work/Work';
+import css from './styles/App.module.scss';
 
 const App = () => {
   //don't forget to add font link in index.html
   return <div className={`bg-primary ${css.container}`}>
     <Header/>
     <Hero/>
+    <Expertise/>
+    <Work/>
 
   </div>
 };
--- src/utils/data.js
+++ src/utils/data.js
@@ -4,50 +4,48 @@ import { MdWorkspacesOutline } from "react-icons/md";
 export const projectExperience = [
   {
     name: "Website Design",
-    projects: 76,
+    projects: 3,
     icon: HiOutlineDesktopComputer,
     bg: "#286F6C",
   },
   {
-    name: "Mobile App Design",
-    projects: 63,
+    name: "App Designs",
+    projects: 2,
     icon: CiMobile1,
     bg: "#EEC048",
   },
   {
-    name: "Brand Identity",
-    projects: 47,
+    name: "Marketing Projects",
+    projects: 3,
     icon: MdWorkspacesOutline,
     bg: "#F26440",
   },
 ];
 
 export const WhatDoIHelp = [
-  "I will help you with finging a solution and solve your problem, We use process design to create digital products. Besides that also help their business.",
-  "We use process design to create digital products. Besides that also help their business",
+  "A first-generation fourth-year student pursuing a degree in Computer Science and minoring in Business at the University of Washington Seattle. A collaborative and a detail-oriented individual, who is searching for internships and research labs related to these skills.",
+  " Always optimistic and enthusiastic about opportunities, ready to take on any challenges and learn from them. Communicative while collaborating with peers and mentors alike to tackle class projects as a team."
 ];
 
 export const workExp = [
   {
-    place: "Self-Employed, Brisbane",
-    tenure: "Aug 2014 - Sep 2016",
-    role: "Visual Designer",
-    detail:
-      "A visual desginer dsesign for a variety of platoforms, may include internet and internet sites, games, movies, kioasks and wearbies. In short, they create the concepts",
+    place: "Springer Capital, Chicago, Illinois",
+    tenure: "Dec 2023 - Apr 2024",
+    role: "Software Engineering Intern",
+    detail: "Implemented Python and SQL to update company software, such as SuiteCRM, and Microsoft Suites (SharePoint, Teams, 365). Written basic scripts to update and maintain the company server and website. Implemented back-end functionality and dded features to track the statuses of hundreds of models through the company's building process, which is used to train machine learning algorithms.",
   },
   {
-    place: "New Man Services",
-    tenure: "Aug 2014 - Sep 2016",
-    role: "UI/UX Designer",
-    detail:
-      "A visual desginer dsesign for a variety of platoforms, may include internet and internet sites, games, movies, kioasks and wearbies. In short, they create the concepts",
+    place: "Husky Coding Project, Seattle, Washington",
+    tenure: "Jan 2023 - Present",
+    role: "Full-stack Developer",
+    detail: "Developing a project called 'Snoozer', supporting high school and college students with different ways to wake up with motivation. Implementing additional features to reduce bugs within the full-stack development using Agile methodologies. Collaborating with a group of 5 to prototype a backend system to enhance human-computer interaction.",
+
   },
   {
-    place: "Global Solution",
-    tenure: "Aug 2014 - Sep 2016",
-    role: "Sr. Product Designer",
-    detail:
-      "A visual desginer dsesign for a variety of platoforms, may include internet and internet sites, games, movies, kioasks and wearbies. In short, they create the concepts",
+    place: "Geeking Out Kids of Color, Seattle, Washington",
+    tenure: "Mar 2022 - Sep 2022",
+    role: "Web Development Intern",
+    detail: "Developed a sound understanding of relevant technologies to help create software applications, ultimately to serve underrepresented communities. Cooperated with other developers to ensure the effective implementation of the technical designs into the resulting software produced. Performed entry-level analysis, problem-solving, implementation, and documentation. Provided technical support to users.",
   },
 ];
 
